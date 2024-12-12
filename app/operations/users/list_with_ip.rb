9ef8b49458ca6363375cd299562d3a140b7db99e
wrmk <elmakan@gmail.com>
2024-12-12 16:34:33 +0600

    refactor logic to get authors with ip

--- app/operations/users/list_with_ip.rb
+++ app/operations/users/list_with_ip.rb
@@ -4,26 +4,17 @@ module Users::ListWithIP
   def self.call(limit)
     return [] if limit.zero?
 
+    users = User.includes(:posts).select("users.id", "users.login").limit(limit)
+
     ips_with_array_of_logins = {}
-    logins_with_ips(limit).each do |hash|
-      ips_with_array_of_logins[hash[:ip]] ||= []
-      ips_with_array_of_logins[hash[:ip]] |= [hash[:login]]
+    users.each do |user|
+      user.posts.each do |post|
+        ip = post.ip.to_s
+        ips_with_array_of_logins[ip] ||= []
+        ips_with_array_of_logins[ip] |= [user.login]
+      end
     end
 
     ips_with_array_of_logins
   end
-
-  def self.logins_with_ips(limit)
-    users = User.includes(:posts).select("users.id", "users.login").limit(limit)
-
-    result = users.map do |user|
-      user.posts.map do |post|
-        { login: user.login, ip: post.ip.to_s }
-      end
-    end
-
-    result.flatten
-  end
-
-  private_class_method :logins_with_ips
 end
